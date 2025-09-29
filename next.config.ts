2bdafb3df40c86227ef4b02c4ca0735ceaa1ccca
raed-sh <raedsh.work@gmail.com>
2025-09-29 22:29:04 +0300

    fix: github action

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  distDir: "dist",
+  distDir: "out",
   basePath: "/GreenCleanSolarEnergy", // TODO: this should follow the domain
 };
 
