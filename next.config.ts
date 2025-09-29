b1c947d1708fdc3d17e0db9188f911fad5223e21
raed-sh <raedsh.work@gmail.com>
2025-09-29 22:13:01 +0300

    refactor: github deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/GreenCleanSolarEnergy", // TODO: this should follow the domain
 };
 
 export default nextConfig;
